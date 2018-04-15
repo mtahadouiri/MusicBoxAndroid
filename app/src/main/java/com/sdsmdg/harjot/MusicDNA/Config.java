7042b94652c3749fce8a6ee652e943403de28e02
mtahadouiri <mohamed.taha.douiri@gmail.com>
2018-04-15 23:26:34 +0100

    Add files via upload

---
+++ app/src/main/java/com/sdsmdg/harjot/MusicDNA/Config.java
@@ -0,0 +1,11 @@
+package com.sdsmdg.harjot.MusicDNA;
+
+/**
+ * Created by Taha on 28/01/2018.
+ */
+
+public class Config {
+    public static final String CLIENT_ID = "";
+    public static final String API_URL = "https://api.soundcloud.com";
+    public static final String GENIUS = "";
+}
